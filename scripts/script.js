80d2162abc114c4b2ec5f019da7bde6df2f8cbd5
nandhinisathish <39171961+nandhinisathish@users.noreply.github.com>
2025-08-19 00:24:17 -0400

    added condition check for pwd

--- scripts/script.js
+++ scripts/script.js
@@ -5,28 +5,43 @@ const confirmPwd = document.getElementById('confirmPwd');
 const email = document.getElementById('email');
 const city = document.getElementById('city');
 const phone = document.getElementById('phone');
+const msg = document.querySelector('span');
 let registerBtn = document.querySelector('button');
 
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    validateUserInputs(e);
+    validateUserInputs();
 })
 
 function validateUserInputs() {
     const uName = username.value;
+    const pwd = password.value;
+    const cpwd = confirmPwd.value;
+   //const phoneValue = phone.value;
     console.log(`username - ${uName}`);
-    if (uName === "") {
-        onFailed(username, "Username cannot be spaces")
-    }else{
-        onSuccess(username);
+    // if (uName === "") {
+    //     onFailed(username, "Username cannot be spaces");
+    // } else {
+    //     onSuccess(username);
+    // }
+    console.log(`pwd`, pwd);
+    console.log(`cpwd`, cpwd);
+    if (pwd === "") {
+        msg.innerText = "Password cannot be spaces";
+        // onFailed(pwd, "Password cannot be spaces");
+    } else if (pwd !== cpwd) {
+        msg.innerText = "Password doesn't match";
+    } else {
+          msg.innerText = "";
+        //onSuccess(pwd);
     }
 }
 
 function onFailed(el, msg) {
     const parentElement = el.parentElement;
-    const displayError = parentElement.querySelector('span')
+    const displayError = parentElement.querySelector("span");
     displayError.innerText = msg;
     parentElement.classList.add('error');
     parentElement.classList.remove('success');
@@ -36,6 +51,7 @@ function onFailed(el, msg) {
 function onSuccess(el) {
     const parentElement = el.parentElement;
     const displayError = parentElement.lastElementchild;
+    displayError.innerText = "";
     parentElement.classList.remove('success');
     parentElement.classList.add('error');
 }
