f386d196fdfb3cc9ee5034ecabaeffb99abe958d
nandhinisathish <39171961+nandhinisathish@users.noreply.github.com>
2025-08-18 21:48:27 -0400

    intial commit with javascript

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let form = document.getElementById('userForm');
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let confirmPwd = document.getElementById('confirmPwd');
+let email = document.getElementById('email');
+let city = document.getElementById('city');
+let phone = document.getElementById('phone');
+let registerBtn = document.querySelector('button');
+
+alert(`username - ${username}`);
